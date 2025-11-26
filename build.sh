ca6b38ffb994c4003e0829639d068419f794fee3
udaydevs <udaysinghno2005@gmail.com>
2025-11-26 19:03:47 +0530

    updated vercel and build.sh file

---
+++ build.sh
@@ -0,0 +1,11 @@
+# Create a virtual environment
+echo "Creating a virtual environment..."
+python3.9 -m venv venv
+source venv/bin/activate
+
+echo "Installing the latest version of pip..."
+python -m pip install --upgrade pip
+
+# Build the project
+echo "Building the project..."
+python -m pip install -r requirements.txt
