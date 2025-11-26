36966cd611f3f4072c53214989190d0847c8167f
udaydevs <udaysinghno2005@gmail.com>
2025-11-26 18:09:22 +0530

    updated requirements.txt and vercel.json

--- ecom/settings.py
+++ ecom/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DJANGO_DEBUG', '') != False
 
-ALLOWED_HOSTS = ['shishi-seven.vercel.app']
+ALLOWED_HOSTS = ['*']
 
 # Application definition
 
